d50e669759309f9f9a610634df7bdbe74be7d35e
jinjiashun <jinjiashun@gmail.com>
2012-07-05 16:14:01 +0800

    Initial Commit

---
+++ BombManDemo/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  BombManDemo
+//
+//  Created by JIA SHUN JIN on 12-7-5.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ BombManDemo/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BombManDemo
+//
+//  Created by JIA SHUN JIN on 12-7-5.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
